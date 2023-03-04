5b9ae2159e28ace2366c15c67608860d0b17c3f6
Marco Celon <20218924+nolecram@users.noreply.github.com>
2023-03-04 14:53:30 +0000

    Test for a new experiment

---
+++ Experiments/Temperature.py
@@ -0,0 +1,9 @@
+# build a main function to collect celsius and convert to fahrenheit
+def main():
+    celsius = float(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+main()
+
+
+
