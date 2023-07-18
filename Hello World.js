20bc72f0d383821bc71d533bacbe0853c1c4d92e
Marco Celon <20218924+nolecram@users.noreply.github.com>
2023-07-18 02:11:00 +0000

    Various updates

--- Hello World.js
+++ Hello World.js
@@ -1,16 +1,4 @@
 document.write("Hello, world!");
 
-function addrandomnumber(min,max){
-    var x = Math.floor(Math.random() * (max - min + 1)) + min;
-    return x;
-}
-// Build a test for the previous function
-function test_addrandomnumber(){
-    var x = addrandomnumber(1,10);
-    if (x >= 1 && x <= 10){
-        return true;
-    }
-    else{
-        return false;
-    }
+
 }
