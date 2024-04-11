dc4d6e24a844b87e26cb525c87fdc19d42d28fde
Marco Celon <20218924+nolecram@users.noreply.github.com>
2024-04-11 12:39:44 +1000

    Update Experiment.py

--- Experiment.py
+++ Experiment.py
@@ -1,7 +1,1 @@
-# Build a program to display nice messagges to the user wife in order to save him when he screw up
-# Define a function to collect the user wife name and return it
-
-def get_wife_name():
-    wife_name = input("Please enter your wife name: ")
-    return wife_name
 
