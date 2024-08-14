ef89acdf5ce3cb39e715d54f47190e59340210d5
Marco Celon <20218924+nolecram@users.noreply.github.com>
2024-08-14 01:37:07 +0000

    chore: Refactor TestA.py and remove unused code

--- Experiments/TestA.py
+++ Experiments/TestA.py
@@ -1,21 +1,9 @@
-# function to convert a number to a binary string
-import unittimport unittest
-from TestA import to_binary
-
-class TestToBinary(unittest.TestCase):
-    def setUp(self):
-        self.test_cases = [
-            (0, '0'),
-            (1, '1'),
-            (2, '10'),
-            (3, '11'),
-            (4, '100'),
-            # Add more test cases as needed
-        ]
-
-    def test_to_binary(self):
-        for input, expected_output in self.test_cases:
-            self.assertEqual(to_binary(input), expected_output)
-
-if __name__ == '__main__':
-    unittest.main()
+# Build a function to convert a number to a binary number
+def main():
+    num = int(input("Enter a number: "))
+    binary = ""
+    while num > 0:
+        binary = str(num % 2) + binary
+        num = num // 2
+    print("The binary representation is", binary)
+main()
