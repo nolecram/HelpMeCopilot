33d774f177d8843cbc1002ccc7eac17ad9b96889
Marco Celon <20218924+nolecram@users.noreply.github.com>
2024-10-24 11:20:57 +1100

    Add deployment of LAMP stack
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/nolecram/HelpMeCopilot?shareId=XXXX-XXXX-XXXX-XXXX).

--- modules/lamp_stack/variables.tf
+++ modules/lamp_stack/variables.tf
@@ -12,3 +12,18 @@ variable "network_interface_id" {
   description = "The ID of the network interface to attach to the LAMP stack VM"
   type        = string
 }
+
+variable "admin_username" {
+  description = "The admin username for the LAMP stack VM"
+  type        = string
+}
+
+variable "public_ip_address" {
+  description = "The public IP address of the LAMP stack VM"
+  type        = string
+}
+
+variable "private_ip_address" {
+  description = "The private IP address of the LAMP stack VM"
+  type        = string
+}
